67cc47720dbee617030efff7ee90e5f4b673d370
Alline.Oliveira <Alline.Oliveira@d22ef09d-d737-ae4e-7486-10adb5fbf1e7>
2010-10-11 20:55:05 +0000

    Creating file: PlayTest.java

---
+++ test/tictactoe/client/userFeatures/PlayTest.java
@@ -0,0 +1,12 @@
+package tictactoe.client.userFeatures.rules;
+
+
+import junit.framework.TestCase;
+/** 
+* Realizes a player move. 
+* 
+* @todo 
+*/
+public class PlayTest extends TestCase {
+
+}
