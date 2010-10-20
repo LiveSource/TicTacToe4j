d83b1beee35bb06ea87f239fad0259f117284a21
Alline.Oliveira <Alline.Oliveira@d22ef09d-d737-ae4e-7486-10adb5fbf1e7>
2010-10-20 21:03:30 +0000

    improved comments for user manual and changed indication of winning sequence to green

---
+++ src/com/codecamp/tictactoe/client/userFeatures/WinningSequence.java
@@ -0,0 +1,45 @@
+package com.codecamp.tictactoe.client.userFeatures;
+
+import com.codecamp.tictactoe.client.TicTacToe;
+
+/**
+ * Highlights the three consecutive marks in a horizontal, vertical, or diagonal
+ * row that won the game.
+ * 
+ * @feature
+ */
+public class WinningSequence {
+
+	public static void showWinner(int currentMoveRow, int currentMoveColumn,
+			String sequenceWinner) {
+
+		String marker = "<font size=6 color=\"green\"><b>"
+				+ TicTacToe.currentPlayer + "</b></font>";
+
+		if (sequenceWinner.equals("row")) {
+
+			GameBoard.gameGrid.setHTML(currentMoveRow, 0, marker);
+			GameBoard.gameGrid.setHTML(currentMoveRow, 1, marker);
+			GameBoard.gameGrid.setHTML(currentMoveRow, 2, marker);
+
+		} else if (sequenceWinner.equals("column")) {
+
+			GameBoard.gameGrid.setHTML(0, currentMoveColumn, marker);
+			GameBoard.gameGrid.setHTML(1, currentMoveColumn, marker);
+			GameBoard.gameGrid.setHTML(2, currentMoveColumn, marker);
+
+		} else if (sequenceWinner.equals("diagonalRight")) {
+
+			GameBoard.gameGrid.setHTML(0, 2, marker);
+			GameBoard.gameGrid.setHTML(1, 1, marker);
+			GameBoard.gameGrid.setHTML(2, 0, marker);
+
+		} else if (sequenceWinner.equals("diagonalLeft")) {
+
+			GameBoard.gameGrid.setHTML(0, 0, marker);
+			GameBoard.gameGrid.setHTML(1, 1, marker);
+			GameBoard.gameGrid.setHTML(2, 2, marker);
+		}
+	}
+
+}
