48e0a20be4d8356ca3ac5e65fb5ae0b8ca63c943
Alline.Oliveira <Alline.Oliveira@d22ef09d-d737-ae4e-7486-10adb5fbf1e7>
2010-10-20 22:43:08 +0000

    Editing file: userFeatures/PlayerLogin.java

--- src/com/codecamp/tictactoe/client/userFeatures/PlayerLogin.java
+++ src/com/codecamp/tictactoe/client/userFeatures/PlayerLogin.java
@@ -2,8 +2,11 @@ package com.codecamp.tictactoe.client.userFeatures;
 
 
 
+
+
 /** 
  * Stops evil getting in.
+TODO: When Player logs on with valid credentials, an empty board game is displayed. 
  * 
  * @TODO
  * @feature 
