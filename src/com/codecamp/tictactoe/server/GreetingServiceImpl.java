62aaa725ad54f33d340e47635d5d56b06200c8fc
Alline.Oliveira <Alline.Oliveira@d22ef09d-d737-ae4e-7486-10adb5fbf1e7>
2010-10-23 19:20:28 +0000

    Editing file: server/GreetingServiceImpl.java

--- src/com/codecamp/tictactoe/server/GreetingServiceImpl.java
+++ src/com/codecamp/tictactoe/server/GreetingServiceImpl.java
@@ -3,8 +3,12 @@ package com.codecamp.tictactoe.server;
 import com.codecamp.tictactoe.client.GreetingService;
 import com.google.gwt.user.server.rpc.RemoteServiceServlet;
 
-/**
+
+
+/** 
  * The server side implementation of the RPC service.
+ * 
+ * @architecture 
  */
 @SuppressWarnings("serial")
 public class GreetingServiceImpl extends RemoteServiceServlet implements
