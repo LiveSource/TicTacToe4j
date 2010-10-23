ac02a3b121edf0546d91a5cdfbc12e7ae7830c6a
joonspoon <joonspoon@d22ef09d-d737-ae4e-7486-10adb5fbf1e7>
2010-10-23 00:10:08 +0000

    Creating file: LoginPage.java

---
+++ src/com/codecamp/tictactoe/client/Authentication/LoginPage.java
@@ -0,0 +1,10 @@
+package com.codecamp.tictactoe.client.Authentication;
+
+/** 
+* When Player logs on with valid credentials, an empty board game is displayed 
+* 
+* @TODO 
+*/
+public class LoginPage {
+
+}
