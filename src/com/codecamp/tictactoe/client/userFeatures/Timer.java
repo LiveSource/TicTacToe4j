4ea834e4ca1d191295a3c02a9e61bf7638d3e160
Alline.Oliveira <Alline.Oliveira@d22ef09d-d737-ae4e-7486-10adb5fbf1e7>
2010-10-23 17:37:59 +0000

    Editing file: client/userFeatures/Timer.java

--- src/com/codecamp/tictactoe/client/userFeatures/Timer.java
+++ src/com/codecamp/tictactoe/client/userFeatures/Timer.java
@@ -1,10 +1,13 @@
 package com.codecamp.tictactoe.client.userFeatures;
 
+
+
 /** 
-* Counts the time passed since the current game started. 
-* 
-* @TODO 
-*/
+ * Counts the time passed since the current game started.
+ * 
+ * @TODO
+ * @feature 
+ */
 public class Timer {
 
 }
