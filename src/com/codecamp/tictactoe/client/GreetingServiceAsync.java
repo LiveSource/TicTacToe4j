6e10b9800890e2fa82caecf117d713d05782bdf0
Alline.Oliveira <Alline.Oliveira@d22ef09d-d737-ae4e-7486-10adb5fbf1e7>
2010-10-23 19:19:46 +0000

    Editing file: client/GreetingServiceAsync.java

--- src/com/codecamp/tictactoe/client/GreetingServiceAsync.java
+++ src/com/codecamp/tictactoe/client/GreetingServiceAsync.java
@@ -4,9 +4,12 @@ import com.google.gwt.user.client.rpc.AsyncCallback;
 
 
 
+
+
 /** 
  * The async counterpart of <code>GreetingService</code>.
- *  
+ * 
+ * @architecture 
  */
 public interface GreetingServiceAsync {
 
