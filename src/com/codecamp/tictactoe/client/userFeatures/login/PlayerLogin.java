7ee92713e57e0e3869d34ed09f5c97c154621343
joonspoon <joonspoon@d22ef09d-d737-ae4e-7486-10adb5fbf1e7>
2010-10-24 20:41:12 +0000

    Creating file: PlayerLogin.java

---
+++ src/com/codecamp/tictactoe/client/userFeatures/login/PlayerLogin.java
@@ -0,0 +1,10 @@
+package com.codecamp.tictactoe.client.userFeatures.login;
+
+/** 
+* When Player logs on with valid credentials, an empty board game is displayed.  
+* 
+* @TODO 
+*/
+public class PlayerLogin {
+
+}
