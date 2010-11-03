0d5633e52a90dbfef4a0ce41742b30bc9bda12c1
Alline.Oliveira <Alline.Oliveira@d22ef09d-d737-ae4e-7486-10adb5fbf1e7>
2010-11-03 06:03:21 +0000

    Creating file: PlayerMoveTest.java

---
+++ test/tictactoe/client/userFeatures/PlayerMoveTest.java
@@ -0,0 +1,26 @@
+package tictactoe.client.userFeatures;
+
+import junit.framework.TestCase;
+import tictactoe.client.userFeatures.PlayerMove;
+
+public class PlayerMoveTest extends TestCase {
+
+	public void testMakeMove() { 
+
+		// GameGrid gameGrid,;
+
+		// HTMLTable.Cell cell;
+
+		// PlayerMove.makeMove(gameGrid,cell);
+
+		assertTrue(true);
+	}
+
+	public void testToggleCurrentPlayer() { 
+
+		// PlayerMove.toggleCurrentPlayer();
+
+		assertTrue(true);
+	}
+
+}
