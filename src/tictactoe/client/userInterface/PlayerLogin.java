28f8f5cef81d32d3dfa87daac7bdc1943cdc99a7
Alline.Oliveira <Alline.Oliveira@d22ef09d-d737-ae4e-7486-10adb5fbf1e7>
2010-11-05 02:27:59 +0000

    Creating file: PlayerLogin.java

---
+++ src/tictactoe/client/userInterface/PlayerLogin.java
@@ -0,0 +1,10 @@
+package tictactoe.client.userInterface;
+
+/** 
+* When Player logs on with valid credentials, an empty board game is displayed.  
+* 
+* @TODO 
+*/
+public class PlayerLogin {
+
+}
