c4d2f9cb4015f1dea15915307a94481d3325287e
Alline.Oliveira <Alline.Oliveira@d22ef09d-d737-ae4e-7486-10adb5fbf1e7>
2010-11-08 19:01:09 +0000

    Creating file: GameInitializationTest.java

---
+++ test/tictactoe/client/GameInitializationTest.java
@@ -0,0 +1,15 @@
+package tictactoe.client;
+
+import junit.framework.TestCase;
+import tictactoe.client.GameInitialization;
+
+public class GameInitializationTest extends TestCase {
+
+	public void testGameInitialization() { 
+
+		// new GameInitialization();
+
+		assertTrue(true);
+	}
+
+}
