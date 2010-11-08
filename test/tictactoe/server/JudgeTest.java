39d3ad8ad3bb5440692aa83e4e011bd4d5ad427a
Alline.Oliveira <Alline.Oliveira@d22ef09d-d737-ae4e-7486-10adb5fbf1e7>
2010-11-08 21:57:24 +0000

    Creating file: JudgeTest.java

---
+++ test/tictactoe/server/JudgeTest.java
@@ -0,0 +1,33 @@
+package tictactoe.server;
+
+import junit.framework.TestCase;
+import tictactoe.server.Judge;
+
+/**
+ * Test Class: Judge.
+ * Define all the rules to identify the winner of the game.
+ *
+ */
+public class JudgeTest extends TestCase {
+
+	public void testJudge() { 
+
+		// new Judge();
+
+		assertTrue(true);
+	}
+
+	public void testCheckForWinner() { 
+
+		// String gameStatus;
+
+		//  int currentMoveRow;
+
+		//  int currentMoveColumn;
+
+		// String checkForWinnerTested = Judge.checkForWinner(gameStatus,currentMoveRow,currentMoveColumn);
+
+// 		assertEquals(checkForWinnerTested, null);
+	}
+
+}
