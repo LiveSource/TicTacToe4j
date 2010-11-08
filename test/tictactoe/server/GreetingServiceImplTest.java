cea47789912f0769b5abd8e810020a0776cb9dbf
Alline.Oliveira <Alline.Oliveira@d22ef09d-d737-ae4e-7486-10adb5fbf1e7>
2010-11-08 22:08:34 +0000

    Creating file: GreetingServiceImplTest.java

---
+++ test/tictactoe/server/GreetingServiceImplTest.java
@@ -0,0 +1,33 @@
+package tictactoe.server;
+
+import junit.framework.TestCase;
+import tictactoe.server.GreetingServiceImpl;
+
+/**
+ * Test Class: GreetingServiceImpl.
+ * The server side implementation of the RPC service.
+ *
+ */
+public class GreetingServiceImplTest extends TestCase {
+
+	public void testGreetingServiceImpl() { 
+
+		// new GreetingServiceImpl();
+
+		assertTrue(true);
+	}
+
+	public void testCheckForWinner() { 
+
+		// String gameStatus;
+
+		//  int currentMoveRow;
+
+		//  int currentMoveColumn;
+
+		// String checkForWinnerTested = new GreetingServiceImpl().checkForWinner(gameStatus,currentMoveRow,currentMoveColumn);
+
+		// assertEquals(checkForWinnerTested, null);
+	}
+
+}
