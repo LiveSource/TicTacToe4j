25e780e7813885fc38390ded642f38606debf362
Alline.Oliveira <Alline.Oliveira@d22ef09d-d737-ae4e-7486-10adb5fbf1e7>
2011-02-05 05:37:29 +0000

    Editing file: client/userInterface/WinningSequence.java

--- src/tictactoe/client/userInterface/WinningSequence.java
+++ src/tictactoe/client/userInterface/WinningSequence.java
@@ -10,12 +10,17 @@ import com.google.gwt.user.client.ui.DecoratedPopupPanel;
 import com.google.gwt.user.client.ui.HTML;
 import com.google.gwt.user.client.ui.Widget;
 
-/**
+
+
+/** 
  * If there is three consecutive marks in a horizontal, vertical, or diagonal
- * series, the current Player wins the game, the system highlights the winning
- * sequence and the game is over.
+ series, the current Player wins the game, the system highlights the winning
+ sequence and the game is over.
+
+TODO: Change the winning sequence color from Red to Green.
  * 
- * @Feature
+ * @TODO
+ * @Feature 
  */
 public class WinningSequence {
 
