7d57068b3a8adf5b59c22c9e31460821d552d806
Alline.Oliveira <Alline.Oliveira@d22ef09d-d737-ae4e-7486-10adb5fbf1e7>
2011-02-05 18:04:06 +0000

    Creating file: GameGridTest.java

---
+++ test/tictactoe/client/userInterface/GameGridTest.java
@@ -0,0 +1,20 @@
+package tictactoe.client.userInterface;
+
+import junit.framework.TestCase;
+import tictactoe.client.userInterface.GameGrid;
+
+/**
+ * Test Class for: GameGrid.
+ * A grid of 3 x 3 cells in wich the players insert marks to indicate their desired moviments.
+ *
+ */
+public class GameGridTest extends TestCase {
+
+	public void testGameGrid() { 
+
+		// new GameGrid();
+
+		assertTrue(true);
+	}
+
+}
