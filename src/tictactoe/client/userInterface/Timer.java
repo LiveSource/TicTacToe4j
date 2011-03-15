d1b1835237b38c11c8b146a48a8a0aa7641b6b48
alline.oliveira <alline.oliveira@d22ef09d-d737-ae4e-7486-10adb5fbf1e7>
2011-03-15 04:26:35 +0000

    Editing file: client/userInterface/Timer.java

--- src/tictactoe/client/userInterface/Timer.java
+++ src/tictactoe/client/userInterface/Timer.java
@@ -1,10 +1,13 @@
 package tictactoe.client.userInterface;
 
+
+
 /** 
-* Counts the time passed since the current game started. 
-* 
-* @TODO 
-*/
+ * Counts the time passed since the current game started.
+ * 
+ * @InProgress
+ * @Requirement 
+ */
 public class Timer {
 
 }
