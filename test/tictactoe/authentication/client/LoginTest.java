9ef0ef7547d2d604a42e9c0a8ec365cc70265b73
alline.oliveira <alline.oliveira@d22ef09d-d737-ae4e-7486-10adb5fbf1e7>
2011-05-12 18:59:43 +0000

    Creating file: LoginTest.java

---
+++ test/tictactoe/authentication/client/LoginTest.java
@@ -0,0 +1,20 @@
+package tictactoe.authentication.client;
+
+import junit.framework.TestCase;
+import tictactoe.authentication.client.Login;
+
+/**
+ * Test Class for: Login.
+ * Form to identify the name and the password of the player.
+ *
+ */
+public class LoginTest extends TestCase {
+
+	public void testLogin() { 
+
+		//  LoginTested = new Login().Login();
+
+		// assertEquals(LoginTested, null);
+	}
+
+}
